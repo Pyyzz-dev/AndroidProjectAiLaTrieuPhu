774dc5a20d0c19cbb1cfeaab7c3c73c84b56041b
Pyyzz <ndphonghe130315@gmail.com>
2020-11-19 15:59:51 +0700

    clean code commit

--- app/src/main/java/com/example/zero/setting/GameSetting.java
+++
@@ -1,7 +0,0 @@
-package com.example.zero.setting;
-
-public class GameSetting {
-    public static String tenLuuTru = "AM_THANH";
-    public static boolean amThanhNen;
-    public static boolean amThanhHieuUng;
-}
